bb50779fbb2fa54e5781d7ae3f8c06bbf983984f
Muriel <muriel.jin@both.com>
2017-09-08 18:01:21 +0800

    添加两点之间的距离（iOS）

--- example/src/examples/polyline.js
+++ example/src/examples/polyline.js
@@ -1,43 +1,35 @@
 import React, {Component} from 'react'
-import {StyleSheet, Alert} from 'react-native'
+import {StyleSheet, Alert,NativeModules} from 'react-native'
 import {MapView, Polyline} from 'react-native-amap3d'
+
+var AMapModel = require('react-native').NativeModules.AMapModel;
+
 
 export default class PolylineExample extends Component {
   static navigationOptions = {
     title: '绘制折线',
   }
+ // _line1 = [
+ //    {
+ //      latitude: 40.006901,
+ //      longitude: 116.097972,
+ //    },
+ //    {
+ //      latitude: 40.006901,
+ //      longitude: 116.597972,
+ //    },
+ //  ]
 
-  constructor(props) {
-      super(props);
-      this.array = [];
-      this.state = {
-          lineArray: [],
-          latitude: 39.9042,
-          longitude: 116.4074,
-      }
-  }
-
-  _line1 = [
-    {
-      latitude: 40.006901,
-      longitude: 116.097972,
-    },
-    {
-      latitude: 40.006901,
-      longitude: 116.597972,
-    },
-  ]
-
-  _line2 = [
-    {
-      latitude: 39.906901,
-      longitude: 116.097972,
-    },
-    {
-      latitude: 39.906901,
-      longitude: 116.597972,
-    },
-  ]
+ //  _line2 = [
+ //    {
+ //      latitude: 39.906901,
+ //      longitude: 116.097972,
+ //    },
+ //    {
+ //      latitude: 39.906901,
+ //      longitude: 116.597972,
+ //    },
+ //  ]
 
    _line3 = [
     {
@@ -57,6 +49,27 @@ export default class PolylineExample extends Component {
       longitude: 121.483278,
     },
   ]
+
+  constructor(props) {
+      super(props);
+      this.array = [];
+      this.state = {
+          lineArray: [],
+          latitude: 39.9042,
+          longitude: 116.4074,
+      }
+  }
+
+  componentDidMount(){
+    // AMapModule.calculateLineDistance({latitude: 39.989612, longitude: 116.480972},{latitude: 39.990347, longitude: 116.480441})
+    AMapModel.calculateLineDistance({latitude: 39.989612, longitude: 116.480972},{latitude: 39.990347, longitude: 116.480441},(error,events) => {
+        if (error) {
+            console.warn(error);
+        } else {
+            alert(events) // 返回的距离 m
+        }
+    });
+  }
 
   _onPress = () => Alert.alert('onPress')
 
--- ios/AMapModel.m
+++ ios/AMapModel.m
@@ -1,10 +1,37 @@
 #import "AMapModel.h"
 
+
 @implementation AMapModel {
 }
+
+// 导出模块，不添加参数即默认为这个类名
+RCT_EXPORT_MODULE();
 
 - (MAOverlayRenderer *)renderer {
     return nil;
 }
+
+
+RCT_EXPORT_METHOD(calculateLineDistance:(NSDictionary *)start
+                                    end:(NSDictionary *)end
+                               resolver:(RCTResponseSenderBlock)callback){
+    //1.将两个经纬度点转成投影点
+    MAMapPoint point1 = MAMapPointForCoordinate(CLLocationCoordinate2DMake([[start objectForKey:@"latitude"] doubleValue],[[start objectForKey:@"longitude"] doubleValue]));
+    MAMapPoint point2 = MAMapPointForCoordinate(CLLocationCoordinate2DMake([[end objectForKey:@"latitude"] doubleValue],[[end objectForKey:@"longitude"] doubleValue]));
+    //2.计算距离
+    CLLocationDistance distance = MAMetersBetweenMapPoints(point1,point2);
+    NSLog(@"---------ddddddd222222222-------%f",distance);
+    NSString *result = [NSString stringWithFormat:@"%f",distance]; //准备回调回去的数据
+    callback(@[[NSNull null],result]);
+}
+
+//// 导出方法，桥接到js的方法返回值类型必须是void
+//RCT_EXPORT_METHOD(doSomething:(NSString *)testStr resolver:(RCTResponseSenderBlock)callback){
+//    NSLog(@"%@ ===> doSomething",testStr);
+//    NSString *callbackData = @"Callback数据"; //准备回调回去的数据
+//    callback(@[[NSNull null],callbackData]);
+//}
+
+
 
 @end
